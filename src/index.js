95c2880fb8e85aabf99cb5cdbad012b1353a4d39
guozhen <guozhen4213@126.com>
2020-04-01 00:05:06 +0800

    打包分析&prefetch

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
-async function getComponent() {
-  const {default:_} = await import(/* webpackChunkName:"lodash"*/ 'lodash')
-  const element = document.createElement('div')
-  element.innerHTML = _.join(['Dell', 'Lee'], '-')
-  return element
-
-  // return import(/* webpackChunkName:"lodash"*/ 'lodash').then(({default:_}) => {
-  //   var element = document.createElement('div')
-  //   element.innerHTML = _.join(['Dell', 'Lee'], '-')
-
-  //   return element
-  // })
-}
-
 document.addEventListener('click',() => {
-  getComponent().then(element => {
-    document.body.appendChild(element)
+  import (/* webpackPrefetch: true */ './click.js').then(({default: func}) => {
+    func()
   })
 })
