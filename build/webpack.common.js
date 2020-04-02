dc9e8de31867c009d870ff008b20df8e8fadedf4
guozhen <guozhen4213@126.com>
2020-04-02 23:05:40 +0800

    shimming 的使用

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-
+const webpack = require('webpack')
 module.exports = {
     entry: {
         main: './src/index.js'
@@ -42,6 +42,10 @@ module.exports = {
             template: 'src/index.html'
         }),
         new CleanWebpackPlugin(),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            _join: ['lodash', 'join']
+        })
     ],
     optimization: {
         // runtimeChunk: {
